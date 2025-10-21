87bd6118bea8c556443b8a1641d4d86ab0faecec
NicoleArias <nicoledayanaariaspinzon@gmail.com>
2025-10-20 19:22:35 -0500

    feat:sparkles:Indexation

--- Dia3/EnclaseIncautos.js
+++ Dia3/EnclaseIncautos.js
@@ -5,3 +5,10 @@ db.createCollection("municipios");
 db.createCollection("unidades");
 db.createCollection("incautaciones");
 
+// La indexación crea una llave guia perteneciente al campo de todos los datos en una columna
+db.departamentos.createIndex({ codDepto: 1 }, { unique: true });
+db.municipios.createIndex({ codMunicipio: 1 }, { unique: true });
+db.unidades.createIndex({ unidad: 1 }, { unique: true });
+db.incautaciones.createIndex({ fechaHecho: 1 });
+db.incautaciones.createIndex({ codMunicipio: 1 });
+
